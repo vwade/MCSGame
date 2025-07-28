895ea5f880121d81f470efd1882919c05cf8e6c5
Victor Wade <p2525657888@gmail.com>
2025-07-28 06:39:21 -0500

    build error

--- Source/MCSGame/TempControllerComponent.cpp
+++ Source/MCSGame/TempControllerComponent.cpp
@@ -1,27 +1,67 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TempControllerComponent.h"
+#include "GameFramework/Pawn.h"  // For input setup if attached to Pawn
 
-// Sets default values
-ATempControllerComponent::ATempControllerComponent()
+UTempControllerComponent::UTempControllerComponent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+    PrimaryComponentTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
-void ATempControllerComponent::BeginPlay()
+void UTempControllerComponent::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+    OwnerActor = GetOwner();
+
+    // Create SpringArm if not set
+    if (!SpringArm) {
+        SpringArm = NewObject<USpringArmComponent>(OwnerActor, TEXT("SpringArm"));
+        SpringArm->RegisterComponent();
+        SpringArm->AttachToComponent(OwnerActor->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+        SpringArm->TargetArmLength = ArmLength;
+        SpringArm->bUsePawnControlRotation = true;
+        SpringArm->bEnableCameraLag = bUseCameraLag;
+    }
+
+    // Create Camera
+    if (!ThirdPersonCamera) {
+        ThirdPersonCamera = NewObject<UCameraComponent>(OwnerActor, TEXT("ThirdPersonCamera"));
+        ThirdPersonCamera->RegisterComponent();
+        ThirdPersonCamera->AttachToComponent(SpringArm, FAttachmentTransformRules::KeepRelativeTransform);
+    }
 }
 
-// Called every frame
-void ATempControllerComponent::Tick(float DeltaTime)
+void UTempControllerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::Tick(DeltaTime);
-
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    // Future: Accumulate heat from movement for dampener expulsion
 }
 
+// Input Functions (Bind these in Pawn's SetupPlayerInputComponent)
+void UTempControllerComponent::MoveForward(float Value)
+{
+    if (OwnerActor && Value != 0.0f) {
+        FVector Forward = OwnerActor->GetActorForwardVector() * Value * ThrustSpeed;
+        OwnerActor->AddActorLocalOffset(Forward, true);  // Sweep for future kinetic-to-heat
+    }
+}
+
+void UTempControllerComponent::MoveRight(float Value)
+{
+    if (OwnerActor && Value != 0.0f) {
+        FVector Right = OwnerActor->GetActorRightVector() * Value * ThrustSpeed;
+        OwnerActor->AddActorLocalOffset(Right, true);
+    }
+}
+
+void UTempControllerComponent::Turn(float Value)
+{
+    if (OwnerActor) {
+        OwnerActor->AddControllerYawInput(Value * RotationSpeed * GetWorld()->GetDeltaSeconds());
+    }
+}
+
+void UTempControllerComponent::LookUp(float Value)
+{
+    if (OwnerActor) {
+        OwnerActor->AddControllerPitchInput(Value * RotationSpeed * GetWorld()->GetDeltaSeconds());
+    }
+}
