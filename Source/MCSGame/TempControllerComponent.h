b330924031763107cdad4f1422178ab213884bb1
Victor Wade <p2525657888@gmail.com>
2025-07-28 06:39:21 -0500

    build error

--- Source/MCSGame/TempControllerComponent.h
+++ Source/MCSGame/TempControllerComponent.h
@@ -1,26 +1,49 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "TempControllerComponent.generated.h"
 
-UCLASS()
-class MCSGAME_API ATempControllerComponent : public AActor
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class MCSGAME_API UTempControllerComponent : public UActorComponent
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ATempControllerComponent();
+    GENERATED_BODY()
+
+public:
+    UTempControllerComponent();
+
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+    virtual void BeginPlay() override;
+
+    // Subcomponents (Created dynamically for modularity)
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USpringArmComponent *SpringArm;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UCameraComponent *ThirdPersonCamera;
+
+    // Movement Vars (Temp; expand to 6DoF with energy costs)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float ThrustSpeed = 1000.0f;  // Scalar for relativistic bursts
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float RotationSpeed = 100.0f;  // For yaw/pitch in orbital maneuvers
+
+    // Camera Vars (Third-person immersion)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    float ArmLength = 500.0f;  // Boom distance (tweak for 1000m craft views)
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    bool bUseCameraLag = true;  // Smooth tracking for kinetic conversions
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Input Bindings (Called from owning Pawn/Actor)
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void Turn(float Value);
+    void LookUp(float Value);
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+    AActor *OwnerActor;  // Cache for movement application
 };
